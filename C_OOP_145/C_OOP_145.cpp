c43ca3daf107d7ce1f01b4b58616c76efafdced8
nrismawardah <nrismawardah@gmail.com>
2023-04-10 10:47:53 +0700

    Membuat metode input pada class matakuliah

--- C_OOP_145/C_OOP_145.cpp
+++ C_OOP_145/C_OOP_145.cpp
@@ -16,4 +16,13 @@ private:
 	string kode;
 	string namaM;
 	int sks;
+public:
+	void input() {
+		cout << "Masukan Kode Matakuliah : ";
+		cin >> kode;
+		cout << "Masukan Nama Matakuliah : ";
+		cin >> namaM;
+		cout << "Jumlah SKS : ";
+		cin >> sks;
+	}
 };
